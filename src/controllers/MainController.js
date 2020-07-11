142cac2aa31b0c778202b16eda184c9464d90b08
RahulKannan12 <rahulgurusumathi5@gmail.com>
2020-07-11 20:18:27 +0530

    Added API Calls and Design Changes

--- src/controllers/MainController.js
+++ src/controllers/MainController.js
@@ -1,12 +1,56 @@
 app.controller('MainController',function($scope, $http){
-    //$scope.welcomeMessage = "Hello ! Welcome to angularJs app";
+    $scope.welcomeMessage = "";
+    let information = {};
     $http({
         method: 'GET',
-        url: 'http://jservice.io/api/clues'
+        url: 'http://jservice.io/api/random'
       }).then(function successCallback(response) {
         $scope.welcomeMessage = response.data[0];
+        $scope.formattedURL = 'https://api.datamuse.com/words?ml=' + $scope.welcomeMessage.answer;
+        $http({
+            method: 'GET',
+            url: $scope.formattedURL
+        }).then(function successCallback(response){
+            $scope.relatedWords1 = response.data[0].word;
+            $scope.relatedWords2 = response.data[1].word;
+            $scope.relatedWords3 = response.data[2].word;
+            information = {
+                questions: [$scope.welcomeMessage.answer, $scope.relatedWords1, $scope.relatedWords2, $scope.relatedWords3],
+                answer: $scope.welcomeMessage.answer, 
+            }
+            $scope.questionsFinal = randomizeArray(information.questions);
+        })
         }, function errorCallback(response) {
           // called asynchronously if an error occurs
           // or server returns response with an error status.
-        });
-})
+        });        
+          
+          let randomizeArray = (arr) =>{
+            let Question = [],
+                till = arr.length; 
+            while(Question.length != till){
+              if(arr.length === 1){
+                Question[Question.length] = arr.splice(0, 1)[0];
+              }else{
+                let random = Math.floor(Math.random()*(arr.length));
+                Question[Question.length] = arr.splice(random, 1)[0];
+              }
+            }
+            return Question;
+          }
+        
+        $scope.checkAnswer = function(type){
+            if($scope.questionsFinal[type] === information.answer){
+                $scope.Result = "Correct!!"
+            }else{
+                $scope.Result = "Wrong!!"
+            }
+        }
+          
+});
+
+app.filter('capitalize', function() {
+    return function(input) {
+      return (angular.isString(input) && input.length > 0) ? input.charAt(0).toUpperCase() + input.substr(1).toLowerCase() : input;
+    }
+});
